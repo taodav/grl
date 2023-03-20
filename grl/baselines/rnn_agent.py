fixed observation overflow bug

grl/baselines/rnn_agent.py
@@ -4,6 +4,8 @@ Relies on haiku's recurrent API (https://dm-haiku.readthedocs.io/en/latest/api.h
 Also based on David Tao's implementation at https://github.com/taodav/uncertainty/blob/main/unc/agents/lstm.py .
 """
 import jax
+# from jax.config import config
+# config.update('jax_disable_jit', True)
 import jax.numpy as jnp
 import numpy as np
 import haiku as hk
@@ -24,7 +26,11 @@ def seq_sarsa_error(q: jnp.ndarray, a: jnp.ndarray, r: jnp.ndarray, g: jnp.ndarr
     First axis of all tensors are the sequence length.
     :return:
     """
+    # print(a.shape)
     target = r + g * q1[jnp.arange(next_a.shape[0]), next_a]
+    # print('r', r)
+    # print('g', g)
+    # print('target', target)
     target = jax.lax.stop_gradient(target)
     q_vals = q[jnp.arange(a.shape[0]), a]
     return q_vals - target
@@ -165,7 +171,8 @@ class RNNAgent(DQNAgent):
     
 def train_rnn_agent(mdp: MDP,
                     agent: DQNAgent,
-                    total_steps: int):
+                    total_steps: int,
+                    zero_obs = False):
     """
     Training loop for a dqn agent.
     :param mdp: mdp to train on. Currently DQN does not support AMDPs.
@@ -177,16 +184,19 @@ def train_rnn_agent(mdp: MDP,
     num_eps = 0
 
     jit_onehot = jax.jit(one_hot, static_argnames=["n"])
+    if zero_obs:
+        jit_onehot = jax.jit(lambda x, y: jnp.zeros((mdp.n_obs,)))
     
     while (steps < total_steps):
         # truncation buffers
         obs, actions, next_obs, terminals, rewards, next_actions = [], [], [], [], [], []
         o_0, _ = mdp.reset()
         o_0_processed = jit_onehot(o_0, mdp.n_obs)
-        obs.append(o_0_processed)
+        
+        a_0 = agent.act(np.array([o_0_processed]))[-1]
         
         for t in range(agent.trunc_len):
-            a_0 = agent.act(np.array(obs))[-1]
+            obs.append(o_0_processed)
             actions.append(a_0)
             o_1, r_0, done, _, _ = mdp.step(a_0, gamma_terminal=False)
             terminals.append(done)
@@ -200,26 +210,30 @@ def train_rnn_agent(mdp: MDP,
             next_actions.append(a_1)
             
             if done:
+                #print(f"Broke early after {t} steps")
                 break
             
             
             
             o_0_processed = o_1_processed
-            obs.append(o_0_processed)
+            a_0 = a_1
             
-        
+            
+       
         batch = JaxBatch(obs=[obs], 
                              actions=[actions], 
                              next_obs=[next_obs], 
                              terminals=[terminals], 
                              rewards=[rewards], 
                              next_actions=[next_actions])
+        if len(batch.obs[0]) > agent.trunc_len:
+            print(f"Batch length was {len(batch.obs[0])}")
+            print(batch)
+            raise ValueError
         loss = agent.update(batch)
            
-
-
         if num_eps % 1000 == 0:
-            print(f"Step {steps} | Episode {num_eps} | Loss {loss} | Q-vals {agent.Qs(batch.obs, agent.network_params)}")
+            print(f"Step {steps} | Episode {num_eps} | Loss {loss} | Obs {batch.obs} | Q-vals {agent.Qs(batch.obs, agent.network_params)}")
         
         num_eps = num_eps + 1
 

tests/baselines/test_rnn.py
@@ -4,10 +4,11 @@ import jax
 from jax.config import config
 import haiku as hk
 from grl.mdp import one_hot
+import numpy as np
 
 config.update('jax_platform_name', 'cpu')
 
-from grl import MDP, environment
+from grl import MDP, AbstractMDP, environment
 from grl.baselines.dqn_agent import DQNArgs
 from grl.baselines.rnn_agent import RNNAgent, train_rnn_agent
 
@@ -15,37 +16,103 @@ from grl.baselines.rnn_agent import RNNAgent, train_rnn_agent
 def test_lstm_chain_pomdp():
     chain_length = 10
     spec = environment.load_spec('po_simple_chain', memory_id=None)
-    n_steps = 5e4 * chain_length
+    n_steps = 1e6 * chain_length
 
     print(f"Testing LSTM with Sequential SARSA on Simple Chain MDP over {n_steps} steps")
     mdp = MDP(spec['T'], spec['R'], spec['p0'], spec['gamma'])
+    pomdp = AbstractMDP(mdp, spec['phi'])
+    print(pomdp.n_obs)
 
     ground_truth_vals = spec['gamma']**jnp.arange(chain_length - 2, -1, -1)
 
     def _lstm_func(x: jnp.ndarray):
         """Unroll an LSTM over sequences of [B, T, F]"""
-        lstm = hk.LSTM(1)
+        init = hk.initializers.VarianceScaling(np.sqrt(2), 'fan_avg', 'uniform')
+        b_init = hk.initializers.Constant(0)
+        lstm = hk.GRU(1, w_i_init=init, 
+                        w_h_init=init, 
+                        b_init=b_init)
         batch_size = x.shape[0]
-        outputs, cell_state = hk.static_unroll(lstm, x, lstm.initial_state(batch_size), time_major=False)
-        return hk.BatchApply(hk.Linear(mdp.n_actions))(outputs), cell_state
+        initial_state = lstm.initial_state(batch_size)
+        initial_state = jnp.zeros(initial_state.shape)
+        outputs, cell_state = hk.dynamic_unroll(lstm, x, initial_state, time_major=False)
+        linear = hk.Linear(pomdp.n_actions, w_init=init, b_init = b_init)
+        return hk.BatchApply(linear)(outputs), cell_state
     
     transformed = hk.without_apply_rng(hk.transform(_lstm_func))
 
     rand_key = random.PRNGKey(2023)
     rand_key, subkey = random.split(rand_key)
-    agent_args = DQNArgs((mdp.n_obs,), mdp.n_actions, mdp.gamma, subkey, algo = "sarsa", trunc_len=chain_length)
+    agent_args = DQNArgs((pomdp.n_obs,), pomdp.n_actions, pomdp.gamma, subkey, algo = "sarsa", trunc_len=chain_length, alpha=0.01)
     agent = RNNAgent(transformed, agent_args)
 
-    agent = train_rnn_agent(mdp, agent, n_steps)
+    agent = train_rnn_agent(pomdp, agent, n_steps, zero_obs=False)
 
 
-    test_batch = jnp.array([[one_hot(s, chain_length) for s in range(chain_length)]])
+    test_batch = jnp.array([[[1.],
+                                [1.],
+                                [1.],
+                                [1.],
+                                [1.],
+                                [1.],
+                                [1.],
+                                [1.],
+                                [1.]]])
     v = jnp.sum(agent.Qs(test_batch, agent.network_params), axis=-1)
 
-    print(f"Calculated values: {v[0][:-1]}\n"
+    print(f"Calculated values: {v[0]}\n"
           f"Ground-truth values: {ground_truth_vals}")
-    assert jnp.all(jnp.isclose(v[0][:-1], ground_truth_vals, atol=0.05))
+    assert jnp.all(jnp.isclose(v[0], ground_truth_vals, atol=0.05))
+
+
+def test_lstm_5len_tmaze():
+    spec = environment.load_spec("tmaze_5_two_thirds_up")
+    trunc_len = 10
+
+    n_steps = 1e6
+
+    print(f"Testing LSTM with Sequential SARSA on 5-length T-maze over {n_steps} steps with trunc length {trunc_len}")
+    mdp = MDP(spec['T'], spec['R'], spec['p0'], spec['gamma'])
+    pomdp = AbstractMDP(mdp, spec['phi'])
+    print(pomdp.n_states)
+    print(pomdp.n_obs)
+
+    # TODO ground truths?
+    #ground_truth_vals = spec['gamma']**jnp.arange(chain_length - 2, -1, -1)
+
+    def _lstm_func(x: jnp.ndarray):
+        """Unroll an LSTM over sequences of [B, T, F]"""
+        init = hk.initializers.VarianceScaling(np.sqrt(2), 'fan_avg', 'uniform')
+        b_init = hk.initializers.Constant(0)
+        lstm = hk.GRU(10, w_i_init=init, 
+                        w_h_init=init, 
+                        b_init=b_init)
+        batch_size = x.shape[0]
+        initial_state = lstm.initial_state(batch_size)
+        #initial_state = jnp.zeros(initial_state.shape)
+        outputs, cell_state = hk.dynamic_unroll(lstm, x, initial_state, time_major=False)
+        linear = hk.Linear(pomdp.n_actions, w_init=init, b_init = b_init)
+        return hk.BatchApply(linear)(outputs), cell_state
+    
+    transformed = hk.without_apply_rng(hk.transform(_lstm_func))
+
+    rand_key = random.PRNGKey(2023)
+    rand_key, subkey = random.split(rand_key)
+    agent_args = DQNArgs((pomdp.n_obs,), pomdp.n_actions, pomdp.gamma, subkey, algo = "sarsa", trunc_len=trunc_len)
+    agent = RNNAgent(transformed, agent_args)
+
+    agent = train_rnn_agent(pomdp, agent, n_steps)
+
+    # TODO how to get ground truths here?
+    # test_batch = jnp.array([[one_hot(0, pomdp.n_obs) for _ in range(trunc_len)]])
+    # v = jnp.sum(agent.Qs(test_batch, agent.network_params), axis=-1)
+
+    
+    # print(f"Calculated values: {v[0][:-1]}\n"
+    #       f"Ground-truth values: {ground_truth_vals}")
+    # assert jnp.all(jnp.isclose(v[0][:-1], ground_truth_vals, atol=0.05))
 
 if __name__ == "__main__":
-    test_lstm_chain_pomdp()
+    #test_lstm_chain_pomdp()
+    test_lstm_5len_tmaze()
 