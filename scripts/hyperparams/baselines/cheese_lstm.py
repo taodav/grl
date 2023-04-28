e5315d9929448946da4ce0bb67c272d80de44795
Nick <nicholas_petrocelli@brown.edu>
2023-04-27 23:45:26 -0400

    cheese hyperparams

---
+++ scripts/hyperparams/baselines/cheese_lstm.py
@@ -0,0 +1,27 @@
+hparams = {
+    'file_name':
+        'cheese_lstm.txt',
+    'entry': 'grl.baselines.run',
+    'args': [{
+        'algo': 'lstm_sarsa',
+        'spec': [
+            # 'tiger-alt-start', 'network',
+            #'tmaze_5_two_thirds_up',
+            # 'example_7', '4x3.95', 
+            'cheese.95',
+            # 'shuttle.95',
+            # 'paint.95'
+            # 'bridge-repair',
+            # 'hallway',
+            #'po_simple_chain'
+        ],
+        'num_updates': int(1.5e5),
+        'start_epsilon': 1.,
+        'epsilon': 0.1,
+        'epsilon_anneal_steps': int(1.5e5 / 4),
+        'alpha': 0.01,
+        'trunc_len': 100, # currently unused
+        'seed': [2020 + i for i in range(10)],
+        'log': None,
+    }]
+}
