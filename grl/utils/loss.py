1ae4bd4a63a946ce47cd317cb115e2bdb118b750
Ruo Yu Tao <ruoyutao@gmail.com>
2024-02-20 13:44:01 -0500

    add next mstde

--- grl/utils/loss.py
+++ grl/utils/loss.py
@@ -515,3 +515,105 @@ def value_error(pi: jnp.ndarray,
 
     return loss, state_vals, expanded_obs_vals
 
+@partial(jit, static_argnames=['error_type', 'residual'])
+def next_step_mstd_err(
+        pi: jnp.ndarray,
+        pomdp: POMDP, # non-state args
+        error_type: str = 'l2',
+        lambda_: float = 0.,
+        residual: bool = False): # initialize static args
+    # First, calculate our TD(0) Q-values
+    v_vals, q_vals, info = lstdq_lambda(pi, pomdp, lambda_=lambda_)
+    vals = {'v': v_vals, 'q': q_vals}
+
+    n_states = pomdp.base_mdp.state_space.n
+    n_obs = pomdp.observation_space.n
+    n_actions = pomdp.action_space.n
+
+    # Project Q-values from observations to states
+    q_soa = q_vals.T[None, :, :].repeat(n_states, axis=0)
+    # q_sa = pomdp.phi @ q_vals.T
+
+    # Calculate all "potential" next q-values by repeating at the front.
+    qp_soasoa = (
+        q_soa[None, None, None, ...]
+        .repeat(n_states, axis=0)
+        .repeat(n_obs, axis=1)
+        .repeat(n_actions, axis=2)
+    )
+
+    # Calculate all current q-values, repeating at the back.
+    q_soasoa = (
+        q_soa[..., None, None, None]
+        .repeat(n_states, axis=-3)
+        .repeat(n_obs, axis=-2)
+        .repeat(n_actions, axis=-1)
+    )
+
+    # Expanded and repeated reward tensor
+    R_sas = jnp.swapaxes(pomdp.base_mdp.R, 0, 1)
+    R_soasoa = (
+        R_sas[:, None, :, :, None, None]
+        .repeat(n_obs, axis=1)
+        .repeat(n_obs, axis=-2)
+        .repeat(n_actions, axis=-1)
+    )
+
+    # Calculate targets (R + gamma * Q') and stop_grad it.
+    targets = R_soasoa + pomdp.gamma * qp_soasoa
+
+    targets = lax.cond(residual, lambda x: x, lambda x: lax.stop_gradient(x), targets)
+
+    # Compute errors
+    tde_soasoa = (targets - q_soasoa)
+
+    # Repeat across front again, for NEXT step TD errors
+    tde_soasoasoa = (
+        tde_soasoa[None, None, None, ...]
+        .repeat(n_states, axis=0)
+        .repeat(n_obs, axis=1)
+        .repeat(n_actions, axis=2)
+    )
+
+    if error_type == 'l2':
+        mag_tde_soasoasoa = (tde_soasoasoa**2)
+    elif error_type == 'abs':
+        mag_tde_soasoasoa = jnp.abs(tde_soasoasoa)
+    else:
+        raise NotImplementedError(f"Error {error_type} not implemented yet in mem_loss fn.")
+
+    # set terminal count to 0 and compute Pr(s)
+    c_s = info['occupancy'] * (1 - pomdp.terminal_mask)
+    pr_s = c_s / c_s.sum()
+
+    # Retrieve Pr(o|s), Pr(s'|s,a)
+    phi_so = pomdp.phi
+    T_sas = jnp.swapaxes(pomdp.base_mdp.T, 0, 1)
+
+    # Compute Pr(s,o,a,s',o',a',s'',o'',a'')
+    pr_s_soasoasoa =            pr_s[   :, None, None, None, None, None, None, None, None] # Pr(s)
+    phi_soasoasoa =           phi_so[   :,    :, None, None, None, None, None, None, None] # Pr(o|s)
+    pi_soasoasoa =                pi[None,    :,    :, None, None, None, None, None, None] # Pr(a|o)
+    T_soasoasoa =              T_sas[   :, None,    :,    :, None, None, None, None, None] # Pr(s'|s,a)
+    next_phi_soasoasoa =      phi_so[None, None, None,    :,    :, None, None, None, None] # Pr(o'|s')
+    next_pi_soasoasoa =           pi[None, None, None, None,    :,    :, None, None, None] # Pr(a'|o')
+    next_T_soasoasoa =         T_sas[None, None, None,    :, None,    :,    :, None, None] # Pr(s''|s',a')
+    next_next_phi_soasoasoa = phi_so[None, None, None, None, None, None,    :,    :, None] # Pr(o''|s'')
+    next_next_pi_soasoasoa =      pi[None, None, None, None, None, None, None,    :,    :] # Pr(a''|o'')
+    # Pr(s,o,a,s',o',a') = Pr(s) *
+    # Pr(o|s) * Pr(a|o) * Pr(s'|s,a) *
+    # Pr(o'|s') * Pr(a'|o') * Pr(s''|s',a') *
+    # Pr(o''|s'') * Pr(a''|o'')
+    pr_soasoasoa = (
+            pr_s_soasoasoa *
+            phi_soasoasoa * pi_soasoasoa * T_soasoasoa *
+            next_phi_soasoasoa * next_pi_soasoasoa * next_T_soasoasoa *
+            next_next_phi_soasoasoa * next_next_pi_soasoasoa
+    )
+
+    # Reweight squared errors according to Pr(s,o,a,s',o',a')
+    weighted_sq_tde_soasoa = pr_soasoasoa * mag_tde_soasoasoa
+
+    # Sum over all dimensions
+    loss = weighted_sq_tde_soasoa.sum()
+    return loss, vals, vals
