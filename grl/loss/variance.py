temporary (?) fix for R(s, s'). Add in runs for variance

grl/loss/variance.py
@@ -14,30 +14,31 @@ def value_second_moment(pi: jnp.ndarray, mdp: Union[POMDP, MDP]):
     Pi_pi = pi.transpose()[..., None]
     T_pi = (Pi_pi * mdp.T).sum(axis=0) # T^π(s'|s)
     R_pi = (Pi_pi * mdp.T * mdp.R).sum(axis=0).sum(axis=-1) # R^π(s)
-
+    gammas = ((1 - mdp.terminal_mask) * mdp.gamma)[None, ...]
     # A*V_pi(s) = b
     # A = (I - \gamma (T^π))
     # b = R^π
-    A = (jnp.eye(mdp.state_space.n) - mdp.gamma * T_pi)
+    A = (jnp.eye(mdp.state_space.n) - gammas * T_pi)
     b = R_pi
     v_pi_s = jnp.linalg.solve(A, b)
 
     R_sa = (mdp.T * mdp.R).sum(axis=-1) # R(s,a)
     q_pi_s = (R_sa + (mdp.gamma * mdp.T @ v_pi_s))
 
+    # TODO: HERE we assume that s' does not determine next reward. I think we need to change R_obs_obs?
+    R_pi_s_s = R_pi[..., None].repeat(mdp.T.shape[-1], axis=-1)  # R(s, s')
 
-    R_pi_s_s = (Pi_pi * mdp.R).sum(axis=0) # S x S
-
-    R_pi_s_s_squared = (Pi_pi * (mdp.R ** 2)).sum(axis=0)
+    R_pi_squared = (Pi_pi * mdp.T * (mdp.R**2)).sum(axis=0).sum(axis=-1) # R^π(s)
+    R_pi_s_s_squared = R_pi_squared[..., None].repeat(mdp.T.shape[-1], axis=-1)  # R(s, s')**2
 
     R_v_s_prime = R_pi_s_s * v_pi_s[None, ...]
-    R_2_pi_s_over_s_prime = T_pi * (R_pi_s_s_squared + 2 * mdp.gamma * R_v_s_prime)
+    R_2_pi_s_over_s_prime = T_pi * (R_pi_s_s_squared + 2 * gammas * R_v_s_prime)
     R_2_pi_s = R_2_pi_s_over_s_prime.sum(axis=-1)
 
     # A*V^{(2)}_pi(s) = b
     # A = (I - \gamma^2 (T_π))
     # b = R^{(2)}_π
-    A = (jnp.eye(mdp.state_space.n) - (mdp.gamma ** 2) * T_pi)
+    A = (jnp.eye(mdp.state_space.n) - (gammas ** 2) * T_pi)
     b = R_2_pi_s
     V_2_pi_s = jnp.linalg.solve(A, b)  # Second moment of V over state
 

scripts/variance_tests.py
@@ -11,8 +11,10 @@ from grl.memory.lib import memory_20 as tmaze_two_goals_optimal_mem
 from grl.loss.variance import get_variances
 from grl.loss import mem_tde_loss, mem_discrep_loss
 
-from grl.environment.policy_lib import switching_two_thirds_right_policy, counting_wall_optimal_memoryless_policy
-
+from grl.environment.policy_lib import (
+    switching_two_thirds_right_policy, counting_wall_optimal_memoryless_policy,
+    switching_og_two_thirds_right_policy
+)
 
 
 if __name__ == "__main__":
@@ -33,6 +35,14 @@ if __name__ == "__main__":
     # This is state-based variance
     if env_str == 'switching':
         pi = switching_two_thirds_right_policy()
+        mem_fn = switching_optimal_deterministic_1_bit_mem()
+        if reward_in_obs:
+            # since reward_in_obs is True
+            mem_fn = np.concatenate((mem_fn, mem_fn[:, -1:]), axis=1)
+            pi = np.concatenate((pi, pi[-1:]), axis=0)
+    elif env_str == 'switching_og':
+        pi = switching_og_two_thirds_right_policy()
+        # TODO: this
         mem_fn = switching_optimal_deterministic_1_bit_mem()
         if reward_in_obs:
             # since reward_in_obs is True

tests/test_sample_td_variance.py
@@ -8,7 +8,7 @@ import numpy as np
 
 from grl.environment import load_pomdp
 from grl.environment.jax_pomdp import load_jax_pomdp, POMDP, LogWrapper, VecEnv
-from grl.environment.policy_lib import switching_two_thirds_right_policy
+from grl.environment.policy_lib import switching_two_thirds_right_policy, counting_wall_optimal_memoryless_policy
 
 from grl.loss.variance import get_variances
 
@@ -57,7 +57,8 @@ def make_collect_samples(env_str: str, env: POMDP,
             pi_arr = switching_two_thirds_right_policy()
             return partial(tabular_policy, pi_arr=pi_arr, epsilon=epsilon), pi_arr
         elif env_str == 'counting_wall':
-            raise NotImplementedError
+            pi_arr = counting_wall_optimal_memoryless_policy()
+            return partial(tabular_policy, pi_arr=pi_arr, epsilon=epsilon), pi_arr
         raise NotImplementedError
 
     policy, pi = get_policy(env_str, epsilon=epsilon)
@@ -74,12 +75,14 @@ def make_collect_samples(env_str: str, env: POMDP,
 
         env_rngs = jax.random.split(rng, n_envs)
         obs, env_state, reward, done, info = env.step(env_rngs, env_state, action, env_params)
+        next_obs = jnp.zeros_like(obs).at[-1].set(1) * done + (1 - done) * obs
         experience = {
             'obs': last_obs,
             'action': action,
             'reward': reward,
             'done': done,
-            'state': state
+            'state': state,
+            'next_obs': next_obs
         }
         runner_state = (env_state, obs, done, rng)
 